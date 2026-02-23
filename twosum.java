7e57f889e1e69ddb0f8bbaac2371d37c047aac85
Kunal <kunaljoshi070@gmail.com>
2026-02-23 22:50:11 +0530

    two sum II

--- twosum.java
+++ twosum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int checksum=0;
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
+           for(int j=i+1; j<nums.length; j++){
                 checksum= nums[i]+nums[j];
                 if(checksum==target){
                     return new int[]{i,j};
@@ -10,5 +10,20 @@ class Solution {
             }
         }
         return null;
-    }
+    } 
+}
+
+//two sum II- for sorted array input
+class sol{
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+           for(int j=i+1; j<nums.length; j++){
+                int checksum = nums[i]+nums[j];
+                if(checksum == target){
+                    return new int[]{i+1,j+1};
+                }
+            }
+        }
+        return null;
+    } 
 }
